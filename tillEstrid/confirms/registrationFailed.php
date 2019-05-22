a7b8e8f9014b8ab00a040bd6c5c14aebf575ed03
LisalinaGM <50692491+LisalinaGM@users.noreply.github.com>
2019-05-22 15:47:58 +0200

    Update registrationFailed.php

--- tillEstrid/confirms/registrationFailed.php
+++ tillEstrid/confirms/registrationFailed.php
@@ -3,10 +3,11 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Register Here</title>
+<title>Registration</title>
 <link href="../assets/css/start.css" rel="stylesheet" type="text/css">
 <script src = "../assets/js/loginValidation.js"></script>
 </head>
+
 <body>
 <div id="mainwrapper">
   <header> 
@@ -15,33 +16,26 @@
   </header>
   <div id="content">
     <section id="mainContent"> 
-      <h1>Register Here!</h1>
-		<p> Fill in your email-address and your password.</p>
-		<p> Your registration failed, try again with another email-address!</p>
-		<form name = "inputForm" action="../processes/registrationProcess.php" onsubmit = "return validateForm()" method = "post">
-			<br><label for="email">Email-address</label>
-			<br><input type="text" id="email" name="email" placeholder="ex. lisa@gmail.com">
-			<br><label for="fname">Password</label>
-			<br><input type="text" id="password" name="password" placeholder="ex. xtlno">
-			<br><input type="submit" value="Register!">
-		</form>
-		<aside id="authorInfo"> 
-		</aside>
+
+      <h1>Registration failed, choose another email-address!</h1>
+     </aside>
     </section>
     <section id="sidebar"> 
-      <input type="text" placeholder="Search">
-      <div id="adimage"><img src="../assets/img/trensstallen.jpg" alt=""/></div>
+      <div id="adimage"><img src="../assets/img/trensstallen.jpg"/></div>
       <nav>
         <ul>
+			<li><a href="../start.php" title="Link">Home</a></li>
+			<li><a href="../searchTip.php" title="Link">Search for Tips</a></li>
           <li><a href="../postTip.php" title="Link">Post a tip</a></li>
           <li><a href="../basilico.php" title="Link">Basilico</a></li>
           <li><a href="../frenchi.php" title="Link">Frenchi</a></li>
           <li><a href="#" title="Link">Iberico</a></li>
           <li><a href="#" title="Link">Aaltos</a></li>
-          <li><a href="../start.php" title="Link">Home</a></li>
+          <li><a href="#" title="Link">Contact us</a></li>
         </ul>
       </nav>
     </section>
+
   </div>
   <div id="footerbar">copyright @ Group 2. All rights reserved</div>
 </div>
