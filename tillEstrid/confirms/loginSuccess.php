b414ea11cb1ea61b082ac629c82236a98345aa92
LisalinaGM <50692491+LisalinaGM@users.noreply.github.com>
2019-05-22 15:43:59 +0200

    Add files via upload

---
+++ tillEstrid/confirms/loginSuccess.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Login</title>
+<link href="../assets/css/start.css" rel="stylesheet" type="text/css">
+<script src = "../assets/js/loginValidation.js"></script>
+</head>
+
+<body>
+<div id="mainwrapper">
+  <header> 
+    <div id="logo"><!-- <img src="logoImage.png" alt="sample logo"> --><!-- Company Logo text -->Logo</div>
+    <nav> <a href="../register.php" title="Link">Sign up</a> <a href="../login.php" title="Link">Log in</a><a href="../loggedOut.php" title="Link">Log out</a> </nav>
+  </header>
+  <div id="content">
+    <section id="mainContent"> 
+      <h1>Welcome, you are now logged in!</h1>
+     </aside>
+    </section>
+    <section id="sidebar"> 
+      <div id="adimage"><img src="../assets/img/trensstallen.jpg"/></div>
+      <nav>
+        <ul>
+			<li><a href="../start.php" title="Link">Home</a></li>
+			<li><a href="../searchTip.php" title="Link">Search for Tips</a></li>
+          <li><a href="../postTip.php" title="Link">Post a tip</a></li>
+          <li><a href="../basilico.php" title="Link">Basilico</a></li>
+          <li><a href="../frenchi.php" title="Link">Frenchi</a></li>
+          <li><a href="#" title="Link">Iberico</a></li>
+          <li><a href="#" title="Link">Aaltos</a></li>
+          <li><a href="#" title="Link">Contact us</a></li>
+        </ul>
+      </nav>
+    </section>
+
+  </div>
+  <div id="footerbar">copyright @ Group 2. All rights reserved</div>
+</div>
+</body>
+</html>
